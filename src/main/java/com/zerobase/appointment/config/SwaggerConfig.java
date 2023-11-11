80beeef17ee0ca03c8515bedd0426f9dfb1ca7e4
KimMunjin <reminisce057@gmail.com>
2023-11-12 04:41:56 +0900

    refactor: swagger 설정 코드 수정

--- src/main/java/com/zerobase/appointment/config/SwaggerConfig.java
+++ src/main/java/com/zerobase/appointment/config/SwaggerConfig.java
@@ -14,11 +14,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    private final String TITLE = "약속 기능 구현 프로젝트";
-    private final String DESCRIPTION ="약속 기능에 필요한 API를 구현한다.";
-    private final String VERSION = "1.0";
     @Bean
     public Docket api(){
+        String title = "약속 기능 구현 프로젝트";
+        String description ="약속 기능에 필요한 API를 구현한다.";
+        String version = "1.0";
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .select()
@@ -26,11 +27,16 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build().apiInfo(apiInfo());
     }
+
     private ApiInfo apiInfo(){
+        String title = "약속 기능 구현 프로젝트";
+        String description ="약속 기능에 필요한 API를 구현한다.";
+        String version = "1.0";
+
         return new ApiInfoBuilder()
-                .title(TITLE)
-                .description(DESCRIPTION)
-                .version(VERSION)
+                .title(title)
+                .description(description)
+                .version(version)
                 .build();
     }
 }
