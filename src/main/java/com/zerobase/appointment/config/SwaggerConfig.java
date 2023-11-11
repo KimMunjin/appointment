fd065597f67f2ffa9b3c350f21dcbc2b16ef4076
KimMunjin <reminisce057@gmail.com>
2023-11-12 04:35:43 +0900

    chore: swagger 설정 추가

---
+++ src/main/java/com/zerobase/appointment/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.zerobase.appointment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private final String TITLE = "약속 기능 구현 프로젝트";
+    private final String DESCRIPTION ="약속 기능에 필요한 API를 구현한다.";
+    private final String VERSION = "1.0";
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zerobase.appointment"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .version(VERSION)
+                .build();
+    }
+}
