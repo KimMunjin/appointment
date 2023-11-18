08bb3342c72e6adb0357e5838836efafed6635ea
KimMunjin <reminisce057@gmail.com>
2023-11-18 22:28:03 +0900

    refactor: 코드 수정

--- src/main/java/com/zerobase/appointment/repository/AppointmentResultRepository.java
+++ src/main/java/com/zerobase/appointment/repository/AppointmentResultRepository.java
@@ -3,7 +3,9 @@ package com.zerobase.appointment.repository;
 import com.zerobase.appointment.entity.AppointmentResult;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppointmentResultRepository extends JpaRepository<AppointmentResult, Long> {
 
   Optional<AppointmentResult> findByAppointmentIdAndParticipantMemberId(Long appointmentId,
